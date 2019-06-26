a852fbdbaf10e28e9a43efa11221108c668e236e
Kirill Gorshkoff <gobzter@gmail.com>
2019-06-26 15:35:58 +0300

    Поправил параметр default_flow_style

--- hw3.py
+++ hw3.py
@@ -11,6 +11,6 @@ def write_to_yaml(path):
     }
 
     with open(path, 'w') as file:
-        yaml.dump(data, file, Dumper=yaml.Dumper, default_flow_style=True, allow_unicode=True)
+        yaml.dump(data, file, Dumper=yaml.Dumper, default_flow_style=False, allow_unicode=True)
 
 write_to_yaml('files/write.yaml')
